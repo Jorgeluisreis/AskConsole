6faa9e06f7f1f2c389f36077a20cc0390d379149
Jorgeluisreis <ctt.jluis@gmail.com>
2025-02-02 23:52:24 -0300

    feat: Agora com frases destacáveis com negrito-itálico.

--- src/main/java/com/jorgeluisreis/askconsole/util/TextFormatter.java
+++ src/main/java/com/jorgeluisreis/askconsole/util/TextFormatter.java
@@ -7,15 +7,27 @@ import static org.fusesource.jansi.Ansi.ansi;
 public class TextFormatter {
 
     public static String formatText(String input) {
+        // Padrão para negrito: **texto**
         Pattern boldPattern = Pattern.compile("\\*\\*(.*?)\\*\\*");
-        Matcher matcher = boldPattern.matcher(input);
-
+        Matcher boldMatcher = boldPattern.matcher(input);
         StringBuffer resultString = new StringBuffer();
 
-        while (matcher.find()) {
-            matcher.appendReplacement(resultString, ansi().bold().a(matcher.group(1)).reset().toString());
+        while (boldMatcher.find()) {
+            boldMatcher.appendReplacement(resultString,
+                    ansi().bold().a(boldMatcher.group(1)).reset().toString());
         }
-        matcher.appendTail(resultString);
+        boldMatcher.appendTail(resultString);
+
+        // Padrão para itálico e negrito: *texto*
+        Pattern italicBoldPattern = Pattern.compile("\\*(.*?)\\*");
+        Matcher italicBoldMatcher = italicBoldPattern.matcher(resultString.toString());
+        resultString.setLength(0); // Resetar o buffer para reutilização
+
+        while (italicBoldMatcher.find()) {
+            italicBoldMatcher.appendReplacement(resultString,
+                    "\033[3m" + ansi().bold().a(italicBoldMatcher.group(1)).reset() + "\033[0m");
+        }
+        italicBoldMatcher.appendTail(resultString);
 
         return resultString.toString();
     }
